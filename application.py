3baa6c7f06d35cc60c892e3429780a662d9e8a62
Daniel Loiola <danf10@me.com>
2019-02-26 14:06:56 +0000

    removed redis

--- application.py
+++ application.py
@@ -73,16 +73,14 @@ def after_request(response):
 
 
 # Configure session to use filesystem (instead of signed cookies)
-#app.config["SESSION_FILE_DIR"] = mkdtemp()
-#app.config["SESSION_PERMANENT"] = False
-#app.config["SESSION_TYPE"] = "filesystem"
-#Session(app)
-
-## DELETE THOSE LINES IF REDIS FAILS TO DO THE LOGIN AND USE THE PREVIOUS ONES
-app.secret_key = '123456789012345678901234'
-app.config['SESSION_TYPE'] = 'redis'
-app.config['SESSION_REDIS'] = redis.from_url('localhost:6379')
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
+
+###############################################################################
+###############################################################################
+
 
 @app.route("/")
 @login_required
