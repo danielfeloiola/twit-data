a5ed01b60ed7cfae3d9921a8b227f53a17bf0163
Daniel Loiola <danf10@me.com>
2022-05-29 17:23:27 -0300

    db debug

--- user.py
+++ user.py
@@ -6,12 +6,12 @@ app = Flask(__name__)
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    hashp = db.Column(db.String(80), unique=True, nullable=False)
-    c_key = db.Column(db.String(25), unique=True, nullable=False)
-    c_secret = db.Column(db.String(50), unique=True, nullable=False)
-    a_token = db.Column(db.String(50), unique=True, nullable=False)
-    a_secret = db.Column(db.String(45), unique=True, nullable=False)
+    username = db.Column(db.String(255), unique=True, nullable=False)
+    hashp = db.Column(db.String(255), unique=True, nullable=False)
+    c_key = db.Column(db.String(255), unique=True, nullable=False)
+    c_secret = db.Column(db.String(255), unique=True, nullable=False)
+    a_token = db.Column(db.String(255), unique=True, nullable=False)
+    a_secret = db.Column(db.String(255), unique=True, nullable=False)
 
     def __init__(self, username, hashp, c_key, c_secret, a_token, a_secret):
         self.username = username
